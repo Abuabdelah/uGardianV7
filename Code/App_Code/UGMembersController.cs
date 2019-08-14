d88ef725a72edb1fdd69c5dfd7b81ee964e730b6
Abuabdellah <ourcms.tuts@gmail.com>
2019-08-14 08:23:08 +0100

    Setup UGardian V7 for Github

---
+++ Code/App_Code/UGMembersController.cs
@@ -0,0 +1,102 @@
+﻿/**
+ * Author r.tarik
+ * Added on 24.05.2019
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Web.WebApi;
+using Umbraco.Core.Services;
+using Umbraco.Core.Models;
+using System.Web.Http;
+
+namespace UGardian.Api
+{
+    public class UGMembersController : UmbracoAuthorizedApiController
+    {
+        //ServiceContext _serviceContext = new Services.MemberService();
+
+        /// <summary>
+        /// Gets a list of all Umbraco members
+        /// </summary>
+        /// <returns>A list of members</returns>
+        public IEnumerable<IMember> GetAllMembers()
+        {
+            var memberService = Services.MemberService;
+            return memberService.GetAllMembers();
+        }
+
+        /// <summary>
+        /// Gets a list of all Umbraco members
+        /// </summary>
+        /// <returns>A list of members</returns>
+        [HttpGet]
+        public object GetAllMembers1()
+        {
+            var memberGroupService = Services.MemberGroupService;
+            var memberService = Services.MemberService;
+            List<IMember> members = new List<IMember>();
+            List<UMember> _members = new List<UMember>();
+           var roles = memberGroupService.GetAll();
+           foreach (var role in roles)
+           {
+               var groupMembers = memberService.GetMembersByGroup(role.Name);
+               if (groupMembers != null && groupMembers.Count() > 0){
+                 foreach(var member in groupMembers){
+              _members.Add(new UMember(){
+                 Id = member.Id,
+                  Key = member.Key,
+                  Name  = member.Name,
+                 Login = member.Username,
+                 LastLogin = member.LastLoginDate,
+                 CreatedOn = member.CreateDate,
+                 UpdatedOn = member.UpdateDate,
+                 Type = member.ContentTypeAlias,
+                 Email = member.Email,
+                 Group = role.Name
+                });
+           }
+               }
+           }
+            int membersCount = 0;
+            members = memberService.GetAll(0, 1000000000,
+            out membersCount).ToList();
+            foreach (var member in members)
+            {
+                int index = _members.FindIndex(item => item.Id == member.Id);
+                if (index < 0)
+                {
+                    _members.Add(new UMember()
+                    {
+                        Id = member.Id,
+                        Key = member.Key,
+                        Name = member.Name,
+                        Login = member.Username,
+                        LastLogin = member.LastLoginDate,
+                        CreatedOn = member.CreateDate,
+                        UpdatedOn = member.UpdateDate,
+                        Type = member.ContentTypeAlias,
+                        Email = member.Email,
+                        Group = ""
+                    });
+                }
+
+            }
+                return _members;
+        }
+
+        /// <summary>
+        /// Gets a list of all Umbraco member roles
+        /// </summary>
+        /// <returns>Returns all roles</returns>
+        [HttpGet]
+        public IEnumerable<string> GetAllRoles()
+        {
+            var memberService = Services.MemberService;
+            return memberService.GetAllRoles();
+        }
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,46 @@
+# [UGardian](https://our.umbraco.com/packages/starter-kits/upillar/)
+
+
+Ugardian is a security plugin to export **Umbraco members** to different formats, filtering records by search and check column visiblity.
+
+## Features
+
+* Copy members to clipboard.
+* Export members as CSV.
+* Export members as Excel.
+* Export members as PDF.
+* Print members.
+* Show members' table by 10, 25, 50, 100 entries.
+* Set column visibility.
+* Sort table by property.
+* Filtering records.
+* Responsive members table.
+* Number of records to export unlimited .
+* Bulk delete.
+
+## Preview
+![UGardian Umbraco Security Plugin, 50%](https://pagure.io/UGardian/raw/master/f/Package/images/ugardian-view.png)
+
+## Getting Started
+
+### Manual installation
+![UGardian manual installation](https://pagure.io/UGardian/raw/master/f/Package/images/Manual%20installation.gif)
+
+
+## Community
+Keep track of development and news.
+
+* Ask, share, learn or give feedback [on the forum support](https://our.umbraco.com/packages/backoffice-extensions/ugardian/support/)
+
+## Backoffice Admin
+
+* Login: Admin - Password: 3214789650
+
+## Thanks to
+
+* [Umbraco](https://our.umbraco.com/)
+* [DataTables](https://datatables.net/)
+
+## License & Copyright
+
+This package is licensed with [Creative Commons Attribution 3.0 License (CC BY 3.0)](http://creativecommons.org/licenses/by/3.0/)
